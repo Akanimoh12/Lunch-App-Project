fc517b74747951d1b0d17ee02613131a80bc6bbf
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-21 02:40:33 +1200

    Profile updated

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile Hello</div>
+    <div>Profile Hello owlrd onn profile</div>
+    <h2>Great Day</h2>
   )
 }
