1ac08632e7cddf15a5fa21b9ec10ab987cc1bdf1
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-21 19:30:03 +1200

    added some components

---
+++ src/components/Profile/Activity.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Activity() {
+    const data = [
+        { name: 'Jenny Wilson', itemNo: 2, date: 'May 18, 2023', itemType: 'Free Lunch', time: '11:25 AM' },
+        { name: 'Akanimoh J.', itemNo: 5, date: 'May 20, 2024', itemType: 'Free Lunch', time: '12:25 PM' },
+        { name: 'Manoah Luka', itemNo: 11, date: 'May 21, 2025', itemType: 'Free Lunch', time: '02:25 PM' },
+        
+    ];
+
+    return (
+        <div>
+
+            {data.map((item, index) => (
+                <div className='grid grid-cols-4 border-b-[0.5px] border-[#87748C] pb-3 ' key={index}>
+
+                    {/* Details of Lunch */}
+                    <div className='col-span-3'>
+                        <h1 className='text-[15px] font-semibold  '>Lunch Received</h1>
+                        <p className='text-[12px] font-medium text-[#87748C] '>From {item.name} </p>
+                        <h4 className='text-[14px] font-medium text-[#87748C]'> {item.date} | {item.time} </h4>
+                    </div>
+
+                    {/* Lunch No. and Specification */}
+                    <div className='col-span-1 text-end content-center  '>
+                        <h1 className='text-[16px] font-medium text-[#7C149B]'>+ {item.itemNo} </h1>
+                        <p className='text-[14px] font-medium text-[#87748C]'>{item.itemType}</p>
+                    </div>
+
+                </div>
+            ))}
+
+        </div>
+    )
+}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Activity from './Activity'
 
 export default function Profile() {
     return (
@@ -11,9 +12,9 @@ export default function Profile() {
                 <div className='grid grid-cols-4'>
                     
                     {/* Profile details and image */}
-                    <div className='col-span-3 '>
+                    <div className='col-span-3 justify-between '>
 
-                        <img src="/rebecaPics.png" alt="" />
+                        <img src="/RebPics.svg" className='w-[78px] h-[78px] ' />
 
                         <div className='mt-[8px]'>
                             <h1 className='text-[#710193] text-[15px] font-extrabold'>Rebecca Adeyoju</h1>
@@ -23,15 +24,81 @@ export default function Profile() {
                     </div>
                     
                     {/* Settings Icon */}
-                    <div>
-                        <h2>Great</h2>
+                    <div className='justify-center w-full justify-items-end '>
+                        <img src="/Vector.svg" className='w-[24px] h-[24px] hover:cursor-pointer  ' />
                     </div>
 
                 </div>
                 {/* End for Profile Section */}
 
+                {/* User Profile Review  */}
+                <div className='flex gap-6'>
+
+                    {/* Total lunches received */}
+                    <div className='mt-[19px]'>
+                        <h1 className='font-extrabold text-[18px] '>87</h1>
+                        <p className='text-[14px] text-[#AD99B2] font-medium '>
+                            Total free <br />
+                            lunches received
+                        </p>
+                    </div>
+
+                    {/* Total lunches given */}
+                    <div className='mt-[19px]'>
+                        <h1 className='font-extrabold text-[18px] '>34</h1>
+                        <p className='text-[14px] text-[#AD99B2] font-medium '>
+                            Total free <br />
+                            lunches given
+                        </p>
+                    </div>
+                    
+
+                </div>
+                {/* End for User Profile Review */}
+
+                {/* Lunch Available */}
+                <div className='bg-[#F0E8F2] rounded-[8px] p-[24px] mt-[24px] '>
+
+                    <div className='flex gap-2'>
+
+                        <img src="Plate.svg" alt="" className='w-[40px] h-[40px]' />
+
+                        <p className='text-[14px] font-medium'>
+                            Lunch <br />
+                            Available 
+                        </p>
+                    </div>
+
+                    <h1 className='text-[32px] font-bold text-[#8524A1] mt-[8px] '>12</h1>
+
+                </div>
+                
+                {/* Redeem Button */}
+                <button className='bg-[#7C149B] font-semibold text-[16px] text-white rounded-[10px] w-[159px] h-[56px] mt-6 hover:cursor-pointer '>Redeem</button>
+
+                {/* Activities Sections */}
+                <div className=''>
+
+                    {/*  */}
+                    <div className='flex justify-between text-[#87748C] my-[24px] text-[16px] font-semibold  '>
+                        <p>Activities</p>
+                        <p className='hover:cursor-pointer '>View All</p>
+                    </div>
+
+                    {/* Content of Activity */}
+                    <div className=''>
+                        <Activity/>
+                        
+                    </div>
+
+                </div>
+                {/* End for Activities Section */}
+
+
+
             </div>
             {/* END FOR CONTAINER */}
+
 
         </div>
 
