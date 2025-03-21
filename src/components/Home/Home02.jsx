42ac7170cd2b6596d82cd92efa5894a532b84f87
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-22 10:14:36 +1200

    updated

--- src/components/Home/Home02.jsx
+++ src/components/Home/Home02.jsx
@@ -5,7 +5,7 @@ export default function Home02() {
         <div className='w-full h-[100vh] sm:justify-center sm:content-center sm:bg-black flex flex-col justify-end '>
 
             {/* CONTAINER */}
-            <div className='sm:w-[390px] w-full sm:m-auto m-0 rounded-2xl p-[24px] sm:border-[1px] sm:border-[#F0E8F2] '>
+            <div className='sm:w-[390px] bg-white w-full sm:m-auto m-0 rounded-2xl p-[24px] sm:border-[1px] sm:border-[#F0E8F2] '>
 
 
                 <div>
@@ -18,7 +18,7 @@ export default function Home02() {
                     <p className='text-[#87748C] font-normal text-[14px]'>
                         <span className='text-[#FFA500] font-semibold text-[16px]'>Note: </span>
                         Your recent achievements have not only impressed me but have also had a positive impact on our entire team.
-                        <br />
+                        <div className='mb-4 w-full'></div>
                         To show my appreciation and recognize your outstanding efforts, I'd like to treat you to a well-deserved 'free lunch.' It's a small token of my admiration for all that you do.
 
                     </p>
@@ -32,7 +32,7 @@ export default function Home02() {
 
                 {/* Redeem Button Section */}
                 <div>
-                <button className='bg-[#7C149B] text-white w-full h-[56px] rounded-[10px] text-[16px] font-semibold mt-16 '>Redeem Lunch</button>
+                    <button className='bg-[#7C149B] text-white w-full h-[56px] rounded-[10px] text-[16px] font-semibold mt-16 hover:cursor-pointer '>Redeem Lunch</button>
                 </div>
 
             </div>
