89d9eda3ecce695203c52b5eb53a1d7ddf708b56
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-23 21:29:45 +0100

    updated

--- src/components/Home/Homee.jsx
+++ src/components/Home/Homee.jsx
@@ -4,21 +4,21 @@ import { useState } from "react";
 import { UserGroupIcon, CogIcon, HomeIcon, UserIcon } from "@heroicons/react/24/outline";
 import Home02 from "./Home02";
 
-    
+
 const Homee = () => {
   const [display, setDisplay] = useState('hidden');
-    const [displayed, setDisplayed] = useState(false);
+  const [displayed, setDisplayed] = useState(false);
 
-    const clickDisplay = () => {
-        if (displayed === false) {
-            setDisplayed(true)
-            setDisplay('block')
-        } else {
-            setDisplayed(false)
-            setDisplay('hidden')
+  const clickDisplay = () => {
+    if (displayed === false) {
+      setDisplayed(true)
+      setDisplay('block')
+    } else {
+      setDisplayed(false)
+      setDisplay('hidden')
 
-        }
     }
+  }
 
   return (
     <div className="flex justify-center items-center h-screen bg-[#FFFCFD] font-sans">
@@ -28,43 +28,43 @@ const Homee = () => {
           <img
             src="/images/Image (1).png"
             alt="Profile"
-            className="w-[32px] h-[32px] rounded-full" 
+            className="w-[32px] h-[32px] rounded-full"
           />
           <div>
             <h2 className="text-[15px] text-[#710193]">Hi, Rebecca!</h2>
             <p className="text-[10px] text-[#AD99B2]">Lab Analyst</p>
           </div>
-          
+
           <div className="flex ml-[170px] ">
-            <img src="/images/Bell.png" alt="" className="w-[24px] h-[24px]"/>
+            <img src="/images/Bell.png" alt="" className="w-[24px] h-[24px]" />
             <img src="/images/Subtract.png" alt="" className="w-[19px] h-[19px]" />
           </div>
         </div>
         <div className="text-[#FFA500] w-[219px] h-[38px] text-[10px]">
-        <i>"your exceptional work deserves the word's 
-        applause and all the rewards it brings"</i>
+          <i>"your exceptional work deserves the word's
+            applause and all the rewards it brings"</i>
         </div>
 
         {/* Lunch Card */}
-        <div className="mb-10">
-        <div className=" text-[#FFFCFD] rounded-lg p-3 text-center mb-3">
-          <img src="/images/Frame 43.png" alt="" className="w-[342px] h-[118px]"/>
-          <p className="text-[11px] mt-2 text-gray-500 rounded-4px border border-dotted h-[29px] border-[#EABA00]">🎁 Congratulations! You received a free lunch. Great job!😍</p>
-        </div>
+        <div className="mb-10 ">
+          <div className=" text-[#FFFCFD] rounded-lg py-3 text-center mb-3 items-center">
+            <img src="/images/Frame 43.png" alt="" className="w-[100%] h-[130px] mb-5 " />
+            <p className="text-[11px] w-full mt-2 text-gray-500 rounded-4px border border-dotted py-[12px] border-[#EABA00]">🎁 Congratulations! You received a free lunch. Great job!😍</p>
+          </div>
 
-        {/* Buttons */}
-        <div className="flex gap-4">
-          <button onClick={clickDisplay} className="flex-1 bg-[#7C149B]  text-white px-3 py-2 rounded-[10px] h-[56px] w-[163px]">
-            Redeem Lunch
-          </button>
-          <button className="flex-1 bg-[#EABA00]  text-white p-[3px] rounded-[10px] h-[56px] w-[163px]">
-            Send Lunch
-          </button>
-        </div>
+          {/* Buttons */}
+          <div className="flex gap-4 mt-7">
+            <button onClick={clickDisplay} className="flex-1 bg-[#7C149B]  text-white px-3 py-2 rounded-[10px] h-[56px] w-[163px]">
+              Redeem Lunch
+            </button>
+            <button className="flex-1 bg-[#EABA00]  text-white p-[3px] rounded-[10px] h-[56px] w-[163px]">
+              Send Lunch
+            </button>
+          </div>
         </div>
 
         {/* Lunch History */}
-        <div className="flex-1 overflow-hidden ">
+        <div className="flex-1 overflow-auto ">
           <div className="flex justify-between items-center mb-4">
             <h4 className="font-semibold text-gray-900">Lunch History</h4>
             <button className="text-[#FFA500] text-[14px]">Received ▼</button>
@@ -83,31 +83,43 @@ const Homee = () => {
                 <p className="text-[10px] font-bold text-[#7C149B]">+ {count}</p>
                 <hr className="border-b border-gray-300 my-3" />
               </div>
-              
+
             ))}
           </div>
         </div>
 
         {/* Footer Navigation */}
         <footer className="flex justify-around pt-2 ">
-      <button className="flex flex-col items-center text-[#7C149B]">
-        <HomeIcon className="h-6 w-6" /> {/* Home icon */}
-        <span className="text-xs">Home</span>
-      </button>
-      <button className="flex flex-col items-center text-gray-500">
-        <UserGroupIcon className="h-6 w-6" /> {/* Employees icon */}
-        <span className="text-xs">Employees</span>
-      </button>
-      <button className="flex flex-col items-center text-gray-500">
-        <UserIcon className="h-6 w-6" /> {/* Profile icon */}
-        <span className="text-xs">Profile</span>
-      </button>
-    </footer>
+          <button className="flex flex-col items-center text-[#7C149B]">
+            <HomeIcon className="h-6 w-6" /> {/* Home icon */}
+            <span className="text-xs">Home</span>
+          </button>
+          <button className="flex flex-col items-center text-gray-500">
+            <UserGroupIcon className="h-6 w-6" /> {/* Employees icon */}
+            <span className="text-xs">Employees</span>
+          </button>
+          <button className="flex flex-col items-center text-gray-500">
+            <UserIcon className="h-6 w-6" /> {/* Profile icon */}
+            <span className="text-xs">Profile</span>
+          </button>
+        </footer>
       </div>
-      < div className={`bg-white sm:w-[390px] bottom-0 absolute w-full sm:m-auto m-0 rounded-2xl p-[24px] sm:border-[1px] sm:border-[#F0E8F2] ${display}  `} >
-      <Home02 />
 
-        </div >
+      <div>
+        
+      </div>
+
+      <div className={`bg-white sm:w-[390px] h-[530px] shadow-gray border-t-[0.5px] shadow bottom-0 absolute w-full sm:m-auto m-0 rounded-2xl p-[24px] sm:border-[1px] sm:border-[#F0E8F2] ${display}  `} >
+
+        {/* Close Button */}
+        <div className=" flex justify-end">
+          <img src="/cancel.svg" alt="" onClick={clickDisplay} className="w-[12px] h-[12px]  " />
+        </div>
+
+        <Home02 />
+
+      </div>
+
     </div>
   );
 };
