d5dd86aef2ddff311ca8c8b45a404b78ceeb9344
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-23 17:15:56 +0100

    updated

--- src/components/Create-Account/signup.jsx
+++ src/components/Create-Account/signup.jsx
@@ -38,11 +38,11 @@ function SignIn() {
       <div className='sm:w-[390px] sm:h-auto h-full w-full sm:m-auto m-0 rounded-2xl p-[24px] sm:border-[1px] sm:border-[#F0E8F2]'>
         <p className='text-[#710193] text-2xl font-sans w-[178px] h-[30px] mt-[49px] flex gap-[8px]'>Sign In</p>
 
-        <div className='flex flex-col gap-2 mt-[50px]'>
-          <div>
-            <p className='text-[#87748C] w-[170px] h-[30px] font-sans mt-2 '>Email</p>
+        <div className=' gap-2 mt-[50px]'>
+          <div className='w-'>
+            <p className='text-[#87748C] h-[30px] font-sans mt-2 '>Email</p>
             <input
-              className='w-[342px] h-[56px] rounded-[10px] border-[0.5px] text-[#87748C] focus:outline-[#87748C]'
+              className='w-full h-[56px] rounded-[10px] border-[0.5px] text-[#87748C] focus:outline-[#87748C]'
               type='text'
               name='email'
               value={formData.email}
@@ -53,9 +53,9 @@ function SignIn() {
           </div>
 
           <div>
-            <p className='text-[#87748C] w-[130px] h-[30px] font-sans mt-2'>Password</p>
+            <p className='text-[#87748C] h-[30px] font-sans mt-2'>Password</p>
             <input
-              className='w-[342px] h-[56px] rounded-[10px] border-[0.5px] text-[#87748C] focus:outline-[#87748C]'
+              className='w-full h-[56px] rounded-[10px] border-[0.5px] text-[#87748C] focus:outline-[#87748C]'
               type='password'
               name='password'
               value={formData.password}
