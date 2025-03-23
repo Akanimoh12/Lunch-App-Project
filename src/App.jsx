1069cb4a241a662095368af81c9e74ede2f84f3e
majormaxx <emersondaniel300@gmail.com>
2025-03-23 12:49:54 +0100

    first half

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import Staff from './components/Create-Account/Staff'
+import Otp from './components/Create-Account/Otp'
 
 export default function App() {
   return (
-    <div className='bg-black text-amber-50'>Hello Word</div>
+    <Otp />
   )
 }
