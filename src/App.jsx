70d1e00815a83ae3304aede90b16206cce188345
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-23 23:54:26 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import React from 'react'
 
 
-import React from 'react'
 import RedeemLunch from './components/Lunch/redeemlunch'
+import Staff from './components/Create-Account/Staff'
+import Otp from './components/Create-Account/Otp'
+import Createaccount from './components/Create-Account/createaccount'
+import Signup from './components/Create-Account/signup'
+import Givefreelunch from './components/Lunch/givefreelunch'
+import Notification from './components/Notification/Notification'
+import Profile from './components/Profile/Profile'
+import Auth from './components/Create-Account/Auth'
+import Home02 from './components/Home/Home02'
+import Homee from './components/Home/Homee'
+import Lunch from './components/Lunch/Lunch'
 
 export default function App() {
   return (
